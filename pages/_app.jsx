fd1c8b9e87ea3d7e7ab9091f6e2f368e477a89d6
hasibnadim <nadimshah206@gmail.com>
2022-03-06 23:14:22 +0600

    types bug fixed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+//import type { AppProps } from 'next/app'
 import '../service/firebase'
 import { useReducer } from 'react'
 import { initialValue, Store } from '../service/Store'
 import { reducer } from '../service/reducer'
 
-function MyApp({ Component, pageProps }: AppProps) {
-
+function MyApp({ Component, pageProps }) {
   return (
     <Store.Provider value={useReducer(reducer, initialValue)}>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = ({
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header User={{ name: 'nadim' }} />
+      <Header />
       <Field News={news} />
     </div>
   )
